276b4e718d7d165e092a668389ec6e0a1640ee19
crazybob <ranweiss463@gmail.com>
2021-04-10 23:34:00 +0300

    Fix typo

--- mint-token/contracts/ImToken.sol
+++ mint-token/contracts/ImToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import "./ERC20.sol";
 
 contract ImToken is ERC20 {
-    constructor() ERC20("iM Token", "RW") {
+    constructor() ERC20("Intelligent Mining Token", "IM") {
         _mint(msg.sender, 90000000000000000000000000);
     }
 }
